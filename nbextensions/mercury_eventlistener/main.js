540b6267a304af0c3f4efcdb17c50f2d41d363b4
saksham219 <saksham2196@gmail.com>
2021-06-18 02:34:28 +0530

    send message from iframe to parent

--- nbextensions/mercury_eventlistener/main.js
+++ nbextensions/mercury_eventlistener/main.js
@@ -1,5 +1,8 @@
 console.log("Loading mercury event listener nbextension")
 
+// send message to parent telling the frame url
+parent.postMessage("Hello", "http://localhost:5000");
+
 window.addEventListener('message', event => {
       // IMPORTANT: check the origin of the data!
       if (event.origin.startsWith('http://localhost')) {
@@ -9,12 +12,12 @@ window.addEventListener('message', event => {
 
           if (data.action == "add_input_cell"){
             if (Jupyter.notebook.ncells() == 0)
-                Jupyter.notebook.insert_cell_above('code').set_text(code);
+                Jupyter.notebook.insert_cell_at_index('code', 0).set_text(code);
             else{
                 if (Jupyter.notebook.get_cell(0).get_text().startsWith("# Mercury-Input"))
                     Jupyter.notebook.get_cell(0).set_text(code);
                 else
-                    Jupyter.notebook.insert_cell_above('code').set_text(code);
+                    Jupyter.notebook.insert_cell_at_index('code', 0).set_text(code);
             }
               console.log(event.data);
               console.log("cell added")
@@ -22,12 +25,12 @@ window.addEventListener('message', event => {
 
           if (data.action == "add_output_cell"){
             if (Jupyter.notebook.ncells() == 0)
-                Jupyter.notebook.insert_cell_below('code').set_text(code);
+                Jupyter.notebook.insert_cell_at_bottom('code').set_text(code);
             else{
                 if (Jupyter.notebook.get_cell(-1).get_text().startsWith("# Mercury-Output"))
                     Jupyter.notebook.get_cell(-1).set_text(code);
                 else
-                    Jupyter.notebook.insert_cell_below('code').set_text(code);
+                    Jupyter.notebook.insert_cell_at_bottom('code').set_text(code);
             }
               console.log(event.data);
               console.log("cell added")            
@@ -38,7 +41,3 @@ window.addEventListener('message', event => {
           return;
       }
 });
-
-// frame = document.getElementById("notebook-iframe")
-// frame.contentWindow.postMessage({"data":{"action": "add_input_cell", "code": "adsadadsA"}}, 
-// "http://localhost:8880/notebooks/work/scripts/Untitled.ipynb");
