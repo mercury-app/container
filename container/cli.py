0952be9022d3d4c4898d1e372d693dee7c53514e
saksham219 <saksham2196@gmail.com>
2021-06-30 21:29:59 +0530

    listen to both nbclient and kernel status and post it to orchestration

--- container/cli.py
+++ container/cli.py
@@ -1,8 +1,12 @@
+from logging import shutdown
 import click
 from notebook.services.contents.filemanager import FileContentsManager as FCM
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
+import os
 
 from container.notebook_kernel import NotebookKernel
-from container.constants import DEFAULT_NOTEBOOK_DIR_PATH
+from container.constants import DEFAULT_NOTEBOOK_DIR_PATH, JUPYTER_RUNTIME_DIR
 
 
 @click.group()
@@ -33,10 +37,52 @@ def write_kernel_variables_to_json(source_outputs, dest_inputs, json):
     dest_inputs = [_.strip() for _ in dest_inputs.split("|")]
     notebook_kernel.write_kernel_variables_to_json(source_outputs, dest_inputs, json)
 
+
 @cli.command()
-def post_kernel_status():
-    noteboook_kernel = NotebookKernel()
-    noteboook_kernel.post_kernel_status()
+@click.option('--client', required=True, help="kernel client- jupyter or nbclient")
+def post_kernel_status(client):
+
+    if client == "nbclient":
+        notebook_kernel = NotebookKernel(client="jupyter")
+        if notebook_kernel._kernel_manager:
+            notebook_kernel.shutdown_kernel()
+
+    while True:
+        kernel_not_found = True
+        while kernel_not_found:
+            notebook_kernel = NotebookKernel(client=client)
+            if notebook_kernel._kernel_manager:
+                kernel_not_found = False
+                break
+
+        notebook_kernel.post_kernel_status()
+        
+
+@cli.command()
+@click.option("--notebook_path", required=True, help="path of notebook to execute")
+def run_notebook(notebook_path):
+    
+    # shut down jupyter clients
+    notebook_kernel = NotebookKernel(client="jupyter")
+    if notebook_kernel._kernel_manager:
+        notebook_kernel.shutdown_kernel()
+
+    with open(notebook_path) as f:
+        nb = nbformat.read(f, as_version=4)
+    
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+
+    try:
+        ep.preprocess(nb, {'metadata': {'path': DEFAULT_NOTEBOOK_DIR_PATH}})
+    except CellExecutionError:
+        out = None
+        msg = f"Error executing the notebook {notebook_path}.\n\n"
+        print(msg)
+        raise
+    finally:
+        print("overwriting notebook")
+        with open(notebook_path, mode='w', encoding='utf-8') as f:
+            nbformat.write(nb, f)
 
 
 if __name__ == "__main__":
--- container/notebook_kernel.py
+++ container/notebook_kernel.py
@@ -1,25 +1,50 @@
+from nbclient import client
 import click
 import json
 import os
 import requests
 import jupyter_client
+import time
 
 from container.constants import JUPYTER_RUNTIME_DIR, HOST_PORT
 
 
 class NotebookKernel:
-    def __init__(self):
-        json_file = [
-            _
-            for _ in os.listdir(JUPYTER_RUNTIME_DIR)
-            if os.path.splitext(_)[1] == ".json" and _.split("-")[0] == "kernel"
-        ]
+    def __init__(self, client=None):
+        self._client = client if client else "jupyter"
+        self._kernel_manager = None
+        assert self._client in ["jupyter", "nbclient"]
 
-        assert len(json_file) == 1, "no/two kernel jsons found"
+        conn_file = None
+        if self._client == "jupyter" and os.path.exists(JUPYTER_RUNTIME_DIR):
+            json_file = [
+                _
+                for _ in os.listdir(JUPYTER_RUNTIME_DIR)
+                if os.path.splitext(_)[1] == ".json" and _.split("-")[0] == "kernel"
+            ]
 
-        cf = jupyter_client.find_connection_file(json_file[0])
-        self._kernel_manager = jupyter_client.BlockingKernelClient(connection_file=cf)
-        self._kernel_manager.load_connection_file()
+            if len(json_file) == 1:
+                conn_file = jupyter_client.find_connection_file(json_file[0])
+
+        if self._client == "nbclient":
+            json_files_in_tmp = [_ for _ in os.listdir('/tmp/') if os.path.splitext(_)[1] == ".json"]
+            if len(json_files_in_tmp)>0:
+                for json_file in json_files_in_tmp:
+                    try:
+                        with open(f"/tmp/{json_file}") as f:
+                            keys_in_json = json.load(f).keys()
+                            if "kernel_name" in keys_in_json:
+                                conn_file = f"/tmp/{json_file}"
+                                break
+                    except:
+                        print("no nbclient exits")
+                        continue
+                
+        if conn_file:
+            print(f"connection file for kernel found at {conn_file}")
+            self._kernel_manager = jupyter_client.BlockingKernelClient(connection_file=conn_file)
+            self._kernel_manager.load_connection_file()
+
 
     def execute_code(self, code) -> dict:
         io_msg_id = self._kernel_manager.execute(code)
@@ -54,24 +79,22 @@ class NotebookKernel:
 
     def post_kernel_status(self):
         while True:
-            json_file = [
-                _ for _ in os.listdir(JUPYTER_RUNTIME_DIR)
-                if os.path.splitext(_)[1] == ".json" and _.split("-")[0] == "kernel"
-            ]
-
-            if len(json_file) == 1:
-                print("kernel start detected")
-                break
-
-        while True:
-
             iopub_msg = self._kernel_manager.get_iopub_msg()
-            
+
+            if "msg_type" in iopub_msg["parent_header"]:
+                if iopub_msg["parent_header"]["msg_type"] == "shutdown_request":
+                    print("Kernel shutdown detected")
+                    # sleep because jupyter takes time in shutting down container
+                    time.sleep(3)
+                    kernel_shutdown = True
+                    return kernel_shutdown
+
             node_id = os.environ.get("MERCURY_NODE")
             url = f"http://host.docker.internal:{HOST_PORT}/nodes/{node_id}/notebook"
 
             if iopub_msg["msg_type"] == "status":
                 kernel_status = iopub_msg["content"]["execution_state"]
+            print('kernel client: ', self._client)
             print('KERNEL STATUS: ', kernel_status)
             
             data = {
@@ -92,3 +115,6 @@ class NotebookKernel:
             response = requests.patch(url, headers=headers, json=data)
             print(response.status_code, response.json())
 
+
+    def shutdown_kernel(self):
+        self._kernel_manager.shutdown()
